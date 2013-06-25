b26992b0e460acb19983996aaeacf876971b7c09
Matt Spitz <mattspitz@gmail.com>
2013-06-25 19:36:27 -0400

    Allow clients to pass in a cookie value for SmartImageUrls.

--- src/com/loopj/android/image/SmartImageView.java
+++ src/com/loopj/android/image/SmartImageView.java
@@ -1,132 +1,152 @@
 package com.loopj.android.image;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class SmartImageView extends ImageView {
-    private static final int LOADING_THREADS = 4;
-    private static ExecutorService threadPool = Executors.newFixedThreadPool(LOADING_THREADS);
+	private static final int LOADING_THREADS = 4;
+	private static ExecutorService threadPool = Executors.newFixedThreadPool(LOADING_THREADS);
 
-    private SmartImageTask currentTask;
+	private SmartImageTask currentTask;
 
+	public SmartImageView(final Context context) {
+		super(context);
+	}
 
-    public SmartImageView(Context context) {
-        super(context);
-    }
+	public SmartImageView(final Context context, final AttributeSet attrs) {
+		super(context, attrs);
+	}
 
-    public SmartImageView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
+	public SmartImageView(final Context context, final AttributeSet attrs, final int defStyle) {
+		super(context, attrs, defStyle);
+	}
 
-    public SmartImageView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
+	// Helpers to set image by URL
+	public void setImageUrl(final String url, final String cookie) {
+		setImage(new WebImage(url, cookie));
+	}
 
+	public void setImageUrl(final String url, final String cookie, final SmartImageTask.OnCompleteListener completeListener) {
+		setImage(new WebImage(url, cookie), completeListener);
+	}
 
-    // Helpers to set image by URL
-    public void setImageUrl(String url) {
-        setImage(new WebImage(url));
-    }
+	public void setImageUrl(final String url, final String cookie, final Integer fallbackResource) {
+		setImage(new WebImage(url, cookie), fallbackResource);
+	}
 
-    public void setImageUrl(String url, SmartImageTask.OnCompleteListener completeListener) {
-        setImage(new WebImage(url), completeListener);
-    }
+	public void setImageUrl(final String url, final String cookie, final Integer fallbackResource, final SmartImageTask.OnCompleteListener completeListener) {
+		setImage(new WebImage(url, cookie), fallbackResource, completeListener);
+	}
 
-    public void setImageUrl(String url, final Integer fallbackResource) {
-        setImage(new WebImage(url), fallbackResource);
-    }
+	public void setImageUrl(final String url, final String cookie, final Integer fallbackResource, final Integer loadingResource) {
+		setImage(new WebImage(url, cookie), fallbackResource, loadingResource);
+	}
 
-    public void setImageUrl(String url, final Integer fallbackResource, SmartImageTask.OnCompleteListener completeListener) {
-        setImage(new WebImage(url), fallbackResource, completeListener);
-    }
+	public void setImageUrl(final String url, final String cookie, final Integer fallbackResource, final Integer loadingResource, final SmartImageTask.OnCompleteListener completeListener) {
+		setImage(new WebImage(url, cookie), fallbackResource, loadingResource, completeListener);
+	}
 
-    public void setImageUrl(String url, final Integer fallbackResource, final Integer loadingResource) {
-        setImage(new WebImage(url), fallbackResource, loadingResource);
-    }
+	public void setImageUrl(final String url) {
+		setImageUrl(url, (String) null);
+	}
 
-    public void setImageUrl(String url, final Integer fallbackResource, final Integer loadingResource, SmartImageTask.OnCompleteListener completeListener) {
-        setImage(new WebImage(url), fallbackResource, loadingResource, completeListener);
-    }
+	public void setImageUrl(final String url, final SmartImageTask.OnCompleteListener completeListener) {
+		setImageUrl(url, (String) null, completeListener);
+	}
 
+	public void setImageUrl(final String url, final Integer fallbackResource) {
+		setImageUrl(url, (String) null, fallbackResource);
+	}
 
-    // Helpers to set image by contact address book id
-    public void setImageContact(long contactId) {
-        setImage(new ContactImage(contactId));
-    }
+	public void setImageUrl(final String url, final Integer fallbackResource, final SmartImageTask.OnCompleteListener completeListener) {
+		setImageUrl(url, (String) null, fallbackResource, completeListener);
+	}
 
-    public void setImageContact(long contactId, final Integer fallbackResource) {
-        setImage(new ContactImage(contactId), fallbackResource);
-    }
+	public void setImageUrl(final String url, final Integer fallbackResource, final Integer loadingResource) {
+		setImageUrl(url, (String) null, fallbackResource, loadingResource);
+	}
 
-    public void setImageContact(long contactId, final Integer fallbackResource, final Integer loadingResource) {
-        setImage(new ContactImage(contactId), fallbackResource, fallbackResource);
-    }
+	public void setImageUrl(final String url, final Integer fallbackResource, final Integer loadingResource, final SmartImageTask.OnCompleteListener completeListener) {
+		setImageUrl(url, (String) null, fallbackResource, loadingResource, completeListener);
+	}
 
+	// Helpers to set image by contact address book id
+	public void setImageContact(final long contactId) {
+		setImage(new ContactImage(contactId));
+	}
 
-    // Set image using SmartImage object
-    public void setImage(final SmartImage image) {
-        setImage(image, null, null, null);
-    }
+	public void setImageContact(final long contactId, final Integer fallbackResource) {
+		setImage(new ContactImage(contactId), fallbackResource);
+	}
 
-    public void setImage(final SmartImage image, final SmartImageTask.OnCompleteListener completeListener) {
-        setImage(image, null, null, completeListener);
-    }
+	public void setImageContact(final long contactId, final Integer fallbackResource, final Integer loadingResource) {
+		setImage(new ContactImage(contactId), fallbackResource, fallbackResource);
+	}
 
-    public void setImage(final SmartImage image, final Integer fallbackResource) {
-        setImage(image, fallbackResource, fallbackResource, null);
-    }
+	// Set image using SmartImage object
+	public void setImage(final SmartImage image) {
+		setImage(image, null, null, null);
+	}
 
-    public void setImage(final SmartImage image, final Integer fallbackResource, SmartImageTask.OnCompleteListener completeListener) {
-        setImage(image, fallbackResource, fallbackResource, completeListener);
-    }
+	public void setImage(final SmartImage image, final SmartImageTask.OnCompleteListener completeListener) {
+		setImage(image, null, null, completeListener);
+	}
 
-    public void setImage(final SmartImage image, final Integer fallbackResource, final Integer loadingResource) {
-        setImage(image, fallbackResource, loadingResource, null);
-    }
+	public void setImage(final SmartImage image, final Integer fallbackResource) {
+		setImage(image, fallbackResource, fallbackResource, null);
+	}
 
-    public void setImage(final SmartImage image, final Integer fallbackResource, final Integer loadingResource, final SmartImageTask.OnCompleteListener completeListener) {
-        // Set a loading resource
-        if(loadingResource != null){
-            setImageResource(loadingResource);
-        }
+	public void setImage(final SmartImage image, final Integer fallbackResource, final SmartImageTask.OnCompleteListener completeListener) {
+		setImage(image, fallbackResource, fallbackResource, completeListener);
+	}
 
-        // Cancel any existing tasks for this image view
-        if(currentTask != null) {
-            currentTask.cancel();
-            currentTask = null;
-        }
+	public void setImage(final SmartImage image, final Integer fallbackResource, final Integer loadingResource) {
+		setImage(image, fallbackResource, loadingResource, null);
+	}
 
-        // Set up the new task
-        currentTask = new SmartImageTask(getContext(), image);
-        currentTask.setOnCompleteHandler(new SmartImageTask.OnCompleteHandler() {
-            @Override
-            public void onComplete(Bitmap bitmap) {
-                if(bitmap != null) {
-                    setImageBitmap(bitmap);
-                } else {
-                    // Set fallback resource
-                    if(fallbackResource != null) {
-                        setImageResource(fallbackResource);
-                    }
-                }
+	public void setImage(final SmartImage image, final Integer fallbackResource, final Integer loadingResource, final SmartImageTask.OnCompleteListener completeListener) {
+		// Set a loading resource
+		if (loadingResource != null) {
+			setImageResource(loadingResource);
+		}
 
-                if(completeListener != null){
-                    completeListener.onComplete();
-                }
-            }
-        });
+		// Cancel any existing tasks for this image view
+		if (currentTask != null) {
+			currentTask.cancel();
+			currentTask = null;
+		}
 
-        // Run the task in a threadpool
-        threadPool.execute(currentTask);
-    }
+		// Set up the new task
+		currentTask = new SmartImageTask(getContext(), image);
+		currentTask.setOnCompleteHandler(new SmartImageTask.OnCompleteHandler() {
+			@Override
+			public void onComplete(final Bitmap bitmap) {
+				if (bitmap != null) {
+					setImageBitmap(bitmap);
+				} else {
+					// Set fallback resource
+					if (fallbackResource != null) {
+						setImageResource(fallbackResource);
+					}
+				}
 
-    public static void cancelAllTasks() {
-        threadPool.shutdownNow();
-        threadPool = Executors.newFixedThreadPool(LOADING_THREADS);
-    }
+				if (completeListener != null) {
+					completeListener.onComplete();
+				}
+			}
+		});
+
+		// Run the task in a threadpool
+		threadPool.execute(currentTask);
+	}
+
+	public static void cancelAllTasks() {
+		threadPool.shutdownNow();
+		threadPool = Executors.newFixedThreadPool(LOADING_THREADS);
+	}
 }
